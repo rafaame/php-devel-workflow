5f6a07ab3327883f2e808ffdfee18bf9dd747190
rafaame <eu@rafaa.me>
2014-12-09 14:28:20 -0200

    Added possibility to create AngularJS projects

--- scripts/staging/setup-project.sh
+++ scripts/staging/setup-project.sh
@@ -27,6 +27,13 @@ confirm()
 cd $(cd -P -- "$(dirname -- "$0")" && pwd -P)
 
 # Deploy the VirtualHost on apache
+echo -n "What kind of project would you like to create? (basic | angularjs): "
+read ptype
+
+vhost_config="apache2-conf-$ptype"
+vhost_skel="vhost-skel-$ptype"
+vhost_githooks="git-hooks-$ptype"
+
 echo -n "Provide a hostname for the project (will be used as ServerName and ServerAlias): "
 read hostname
 
@@ -61,13 +68,13 @@ fi
 awk_args="{
     sub(/PORT/,\"$port\");
     sub(/SERVERNAME/,\"$hostname\");
-    sub(/SERVERALIAS/,\"$hostname\");
+    sub(/SERVERALIAS/,\"www.$hostname\");
     sub(/DOCROOT/, \"$vhostdir/$hostname\");
     sub(/USER/, \"$vhostuser $vhostuser\");
     print
 }"
 
-cat apache2-conf/virtual-host | awk "$awk_args" > "$available/$hostname"
+cat "$vhost_config/virtual-host" | awk "$awk_args" > "$available/$hostname"
 grep $hostname /etc/hosts
 if [ $? -eq 0 ]
 then
@@ -75,6 +82,7 @@ then
 else
     if [ "$port" != "80" ]
     then
+        #FIXME: check if angularjs and add all hosts
         echo "127.0.0.1:$port $hostname" >> /etc/hosts
     else
         echo "127.0.0.1 $hostname" >> /etc/hosts
@@ -87,14 +95,14 @@ if [ $? != 0 ]
 then
 
     mkdir -p $vhostdir/$hostname/
-    cp -R vhost-skel/* $vhostdir/$hostname/
+    cp -R "$vhost_skel"/* $vhostdir/$hostname/
 
     awk_args="{
         sub(/_SESSION_DIR/,\"$vhostdir/$hostname/tmp\");
         print
     }"
 
-    cat vhost-skel/fcgi-bin/php.ini | awk "$awk_args" > $vhostdir/$hostname/fcgi-bin/php.ini
+    cat "$vhost_skel"/fcgi-bin/php.ini | awk "$awk_args" > $vhostdir/$hostname/fcgi-bin/php.ini
 
     chown -R $vhostuser:$vhostuser $vhostdir/$hostname/
 
@@ -104,32 +112,84 @@ then
     if [ $? != 0 ]
     then
 
-        git init --bare $vhostdir/$hostname/repository.git
+        case "$ptype" in
 
-        echo -n "Provide the directory (relative to /var/www/example.com/) that the files will be copied from the repository (httpdocs): "
-        read deploydir
-        if [ -z "$deploydir" ]
-        then
-            deploydir="httpdocs"
-        fi
+            basic) 
+            
+                git init --bare $vhostdir/$hostname/repository.git
 
-        #echo -n "Provide the command that will be used to import the database on the post-receive git hook. It will be used as "\$command < .database_dump". Default: (mysql -u [user] -p[password] [database]): "
-        #read importdbcommand
-        #if [ -z "$importdbcommand" ]
-        #then
-        #    importdbcommand="mysql -u root -p dbtest"
-        #fi
+                echo -n "Please enter the API database update command that will be executed after an API deploy (Zend:'php httpdocs/index.php orm:schema-tool:update --force'; Laravel: 'php artisan doctrine:schema:update'): "
+                read api_database_update_command
 
-        awk_args="{
-            sub(/_APPLICATION_DIR/,\"$deploydir\");
-            sub(/_CHOWN/,\"$vhostuser:$vhostuser\");
-            sub(/_IMPORT_DATABASE_COMMAND/,\"$importdbcommand\");
-            print
-        }"
+                if [ -z "$api_database_update_command" ]
+                then
+                    api_database_update_command="echo -n \\\"No database update command provided. Skipping.\\\""
+                fi
 
-        cat git-hooks/post-receive | awk "$awk_args" > $vhostdir/$hostname/repository.git/hooks/post-receive
+                echo -n "Provide the directory (relative to /var/www/example.com/) that the files will be copied from the repository (httpdocs): "
+                read deploydir
 
-        chmod 755 $vhostdir/$hostname/repository.git/hooks/post-receive
+                if [ -z "$deploydir" ]
+                then
+                    deploydir="httpdocs"
+                fi
+
+                awk_args="{
+                    sub(/_APPLICATION_DIR/,\"$deploydir\");
+                    sub(/_CHOWN/,\"$vhostuser:$vhostuser\");
+                    sub(/_DATABASE_UPDATE_COMMAND/, \"$api_database_update_command\");
+                    print
+                }"
+
+                cat "$vhost_githooks"/post-receive | awk "$awk_args" > $vhostdir/$hostname/repository.git/hooks/post-receive
+
+                chmod +x $vhostdir/$hostname/repository.git/hooks/post-receive
+
+                ;;
+
+            angularjs)
+
+                git init --bare $vhostdir/$hostname/app/repository.git
+                git init --bare $vhostdir/$hostname/admin/repository.git
+                git init --bare $vhostdir/$hostname/api/repository.git
+                git init --bare $vhostdir/$hostname/common/repository.git
+
+                echo -n "Please enter the API database update command that will be executed after an API deploy (Zend:'php httpdocs/index.php orm:schema-tool:update --force'; Laravel: 'php artisan doctrine:schema:update'): "
+                read api_database_update_command
+
+                if [ -z "$api_database_update_command" ]
+                then
+                    api_database_update_command="echo -n \\\"No database update command provided. Skipping.\\\""
+                fi
+
+                deploydir="."
+
+                awk_args="{
+                    sub(/_APPLICATION_DIR/,\"$deploydir\");
+                    sub(/_CHOWN/,\"$vhostuser:$vhostuser\");
+                    sub(/_DATABASE_UPDATE_COMMAND/, \"$api_database_update_command\");
+                    print
+                }"
+
+                cat "$vhost_githooks"/post-receive-angularjs | awk "$awk_args" > $vhostdir/$hostname/app/repository.git/hooks/post-receive
+                cat "$vhost_githooks"/post-receive-angularjs | awk "$awk_args" > $vhostdir/$hostname/admin/repository.git/hooks/post-receive
+                cat "$vhost_githooks"/post-receive-api | awk "$awk_args" > $vhostdir/$hostname/api/repository.git/hooks/post-receive
+                cat "$vhost_githooks"/post-receive-common | awk "$awk_args" > $vhostdir/$hostname/common/repository.git/hooks/post-receive
+
+                chmod +x $vhostdir/$hostname/app/repository.git/hooks/post-receive
+                chmod +x $vhostdir/$hostname/admin/repository.git/hooks/post-receive
+                chmod +x $vhostdir/$hostname/api/repository.git/hooks/post-receive
+                chmod +x $vhostdir/$hostname/common/repository.git/hooks/post-receive
+
+                ;;
+
+            *)
+
+                echo -n "Wrong project type choosen."
+
+                ;;
+
+        esac
 
     fi
 
@@ -137,10 +197,10 @@ fi
 
 echo "Setting up permissions..."
 chown -R $vhostuser:$vhostuser $vhostdir/$hostname
-chmod 755 $vhostdir/$hostname/fcgi-bin/php5-fcgi
+chmod +x $vhostdir/$hostname/fcgi-bin/php5-fcgi
 
 a2ensite "$hostname"
 echo "Reloading apache2..."
 /etc/init.d/apache2 reload
 
-echo "Your virtual host was created!"
+echo "Your project was created!"
